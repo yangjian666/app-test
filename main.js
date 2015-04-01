ccc356d16896f291481775188077ffda8852fd16
yangjian <yangjian_666@qq.com>
2015-04-01 15:18:48 +0800

    first
    test

---
+++ main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by lanjian on 15/4/1.
+ */
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:1337/');
